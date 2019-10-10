ff18ec455f6aa718440907a302527ed396335db5
RosyRivas <rivas-rossy@hotmail.com.ar>
2019-10-10 16:05:45 -0300

    primer clase de persona terminado

--- PochocloCritics/src/main/java/org/pochocloapps/pochoclocritics/Persona.java
+++ PochocloCritics/src/main/java/org/pochocloapps/pochoclocritics/Persona.java
@@ -5,10 +5,54 @@
  */
 package org.pochocloapps.pochoclocritics;
 
+import java.util.Date;
+
 /**
  *
  * @author Rosi-PC
  */
 public class Persona {
+    private Long idPersona;
+    private String nombre;
+    private String apellido;
+    private Date fechaNac;
+    
+    
+    public Persona (Long idPersona,String nombre,String apellido, Date fechaNac){
+        this.idPersona = idPersona;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaNac = fechaNac;
+    
+    }
+
+    public Long getIdPersona() {
+        return idPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public Date getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(Date fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+    
     
 }
