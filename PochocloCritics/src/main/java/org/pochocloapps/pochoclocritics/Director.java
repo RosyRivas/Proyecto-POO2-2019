2f13096c10fd30cfc1279e2ee19ca8cbaa945f64
OsvaldoEmanuel <osvaldozakowicz@gmail.com>
2019-10-17 15:54:27 -0300

    Clase Director correccion de la documentacion

--- PochocloCritics/src/main/java/org/pochocloapps/pochoclocritics/Director.java
+++ PochocloCritics/src/main/java/org/pochocloapps/pochoclocritics/Director.java
@@ -16,9 +16,9 @@ public class Director {
    // private List<Peliculas> peliculasDirigidas;
    //@param peliculaDirigida lista de las peliculas que dirigidas 
     /**
-     *Crea un nuevo Directos
-     * @param idDirector identificador del director 
-     * @param biografia biografia del Director
+     *Crea un nuevo Director en el sistema
+     * @param idDirector, identificador del director 
+     * @param biografia, biografia del director
      * 
      */
     public Director(Long idDirector, String biografia) {
@@ -28,7 +28,7 @@ public class Director {
 
     /**
      * Metodo que devuelve el identificador del director
-     * @return identificador del director 
+     * @return idDirector, identificador del director 
      * 
      */
     public Long getIdDirector() {
@@ -37,7 +37,7 @@ public class Director {
 
     /**
     *Metodo que permite la modificacion de la biografia del Director 
-    * @return biografia del director
+    * @return biografia, breve descripción del director
     * 
     */
     public String getBiografia() {
@@ -45,14 +45,18 @@ public class Director {
     }
     /**
     *Metodo que permite modificar la biografia del director
-    * @param biografia del Director, incluye una descripcion del director
+    * @param biografia, breve descripción del director
     */
     public void setBiografia(String biografia) {
         this.biografia = biografia;
     }
-    
-    
-    
-    
-    
+    /**
+     * Metodo que devuelve la información de un director
+     * @return idDirector, biografía - Identificador del director y su biografía.
+    */
+    @Override
+    public String toString() {
+        return "Director{" + "idDirector=" + idDirector + ", biografia=" + biografia + '}';
+    }
+      
 }
