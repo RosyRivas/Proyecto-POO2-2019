6e86052e1fcdd62470b539f14996dbf4b1ea52b5
OsvaldoEmanuel <osvaldozakowicz@gmail.com>
2019-10-17 09:50:44 -0300

    Clase Actor documentada

--- PochocloCritics/src/main/java/org/pochocloapps/pochoclocritics/Actor.java
+++ PochocloCritics/src/main/java/org/pochocloapps/pochoclocritics/Actor.java
@@ -14,28 +14,35 @@ public class Actor {
     private String biografia;
    // private List<Peliculas> peliculasActuadas;
 
+    /**
+    * Crea un nuevo actor
+    * @param id del actor
+    * @param biografia del actor
+    */
     public Actor(Long idActor, String biografia) {
         this.idActor = idActor;
         this.biografia = biografia;
     }
-
+    /** 
+    * Método que develve el identificador (id) de un actor
+    * @return id del actor
+    */
     public Long getIdActor() {
         return idActor;
     }
-
-    public void setIdActor(Long idActor) {
-        this.idActor = idActor;
-    }
-
+    /**
+    * Método que devuelve la biografía de un actor
+    * @return biografía del actor
+    */
     public String getBiografia() {
         return biografia;
     }
-
+    /**
+    * Método que crea la biografía de un actor
+    * @param biografia del actor, que incluye una descripción breve de el actor.
+    */
     public void setBiografia(String biografia) {
         this.biografia = biografia;
     }
-    
-    
-    
     
 }
