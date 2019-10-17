5debf9d518c6d5cf2051e37efc2cded0365bdeab
OsvaldoEmanuel <osvaldozakowicz@gmail.com>
2019-10-17 11:54:10 -0300

    Clase Pelicula Documentada

--- PochocloCritics/src/main/java/org/pochocloapps/pochoclocritics/Pelicula.java
+++ PochocloCritics/src/main/java/org/pochocloapps/pochoclocritics/Pelicula.java
@@ -19,11 +19,18 @@ public class Pelicula {
     private Date duracion;
     private String sinopsis;
      private List<String> genero;
-//private Imagen portada;
-   
+    //private Imagen portada;
     // private Actor actor;
     //private Director director;
-
+     
+    /**
+     * Crea una nueva película
+     * @param idPelicula, identificador de una película
+     * @param titulo, titulo de la película
+     * @param duracion, tiempo de reproducción de la pelicula
+     * @param sinopsis, descripción breve de la pelicula
+     * @param genero, listado de géneros de la pelicula
+    */
     public Pelicula(Long idPelicula, String titulo, Date duracion, String sinopsis, List<String> genero) {
         this.idPelicula = idPelicula;
         this.titulo = titulo;
@@ -31,43 +38,76 @@ public class Pelicula {
         this.sinopsis = sinopsis;
         this.genero = genero;
     }
-
+    /**
+     * Método que permite obtener el identificador de la película
+     * @return idPelicula
+    */
     public Long getIdPelicula() {
         return idPelicula;
     }
-
-    
-
+    /**
+     * Método que permite obtener el titulo de la película
+     * @return titulo
+    */
     public String getTitulo() {
         return titulo;
     }
-
+    /**
+     * Método que permite crear el título de la película
+     * @param titulo
+    */
     public void setTitulo(String titulo) {
         this.titulo = titulo;
     }
-
+    /**
+     * Método que permite obtener la duración de la película
+     * @return duracion
+    */
     public Date getDuracion() {
         return duracion;
     }
-
+    /**
+     * Modo que prmite crear la duración de una película
+     * @param duracion
+    */
     public void setDuracion(Date duracion) {
         this.duracion = duracion;
     }
-
+    /**
+     * Método que permite obtener la sinopsis de una película
+     * @return sinopsis
+    */
     public String getSinopsis() {
         return sinopsis;
     }
-
+    /**
+     * Método que permite crear la sinopsis de una pelicula
+     * @param sinopsis
+    */
     public void setSinopsis(String sinopsis) {
         this.sinopsis = sinopsis;
     }
-
+    /**
+     * Método que permite cargar un nuevo genero en una película
+     * @param genero
+    */
     public void setGenero(List<String> genero) {
         this.genero = genero;
     }
-
+    /**
+     * Método que permite obtener los géneros de una película
+     * @return genero
+    */
     public List<String> getGenero() {
         return genero;
+    }
+    /**
+     * Método que devuelve la información de una película
+     * @return idPelicula, titulo, duracion, sinopsis, genero
+    */
+    @Override
+    public String toString() {
+        return "Pelicula{" + "idPelicula=" + idPelicula + ", titulo=" + titulo + ", duracion=" + duracion + ", sinopsis=" + sinopsis + ", genero=" + genero + '}';
     }
     
 }
