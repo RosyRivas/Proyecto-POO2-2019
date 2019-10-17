f19b0b15250a7592c51aaf703f3f45eb1466390f
OsvaldoEmanuel <osvaldozakowicz@gmail.com>
2019-10-17 11:53:49 -0300

    Clase Moderador Documentada

--- PochocloCritics/src/main/java/org/pochocloapps/pochoclocritics/Moderador.java
+++ PochocloCritics/src/main/java/org/pochocloapps/pochoclocritics/Moderador.java
@@ -10,12 +10,40 @@ package org.pochocloapps.pochoclocritics;
  * @author Rosi-PC
  */
 public class Moderador {
-    private String privilegios;
+    private Long idModerador;
+    private String privilegios; //explica lo que puede hacer un moderador, está predefinido
+    private Long contraseñaModerador; //indica una clave de moderador, habilita acciónes de moderador
 
-    public Moderador(String privilegios) {
-        this.privilegios = privilegios;
+    /**
+     * Crea un nuevo moderador del sistema
+     * @param idModerador identificador del moderador
+     * @param contraseñaModerador contraseña 
+    */
+    public Moderador(Long idModerador, Long contraseñaModerador) {
+        this.idModerador = idModerador;
+        this.contraseñaModerador = contraseñaModerador;
     }
-    
+    /**
+     * Método que permite obtener los privilegios de un moderador
+     * @return privilegios
+    */
+    public String getPrivilegios() {
+        return privilegios;
+    }
+    /**
+     * Método que permite obtener la contraseña del moderador
+     * @return contraseñaModerador;
+    */
+    public Long getContraseñaModerador() {
+        return contraseñaModerador;
+    }
+    /**
+     * Método que permite crear una contraseña del moderador
+     * @param contraseñaModerador
+    */
+    public void setContraseñaModerador(Long contraseñaModerador) {
+        this.contraseñaModerador = contraseñaModerador;
+    }
     
     public void administrarPelicula(){
     
@@ -23,7 +51,13 @@ public class Moderador {
     public void añadirElenco(){
     
     }
-    
-    
+    /**
+     * Método que devuelve información del moderador
+     * @return idModerador, privilegios
+    */
+    @Override
+    public String toString() {
+        return "Moderador{" + "idModerador=" + idModerador + ", privilegios=" + privilegios + '}';
+    }
     
 }
