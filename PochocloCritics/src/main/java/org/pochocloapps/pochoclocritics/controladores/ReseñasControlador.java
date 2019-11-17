2c60cd1f6b69eb3ab7d06cffde6d53d4f0fe7b3e
RosyRivas <rivas-rossy@hotmail.com.ar>
2019-11-17 02:10:45 -0300

    se realizo cambios añadiendo el controlador y repositorio de reseñas

---
+++ PochocloCritics/src/main/java/org/pochocloapps/pochoclocritics/controladores/ReseñasControlador.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pochocloapps.pochoclocritics.controladores;
+
+import io.javalin.http.Context;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import org.pochocloapps.pochoclocritics.modelos.Reseña;
+import org.pochocloapps.pochoclocritics.repositorios.ReseñaNoEncontradoException;
+import org.pochocloapps.pochoclocritics.repositorios.ReseñasRepositorio;
+import org.pochocloapps.pochoclocritics.server.ReseñaNoEncontradoExcepcion;
+
+
+
+/**
+ *
+ * @author Rosi-PC
+ */
+public class ReseñasControlador {
+   
+         private final ReseñasRepositorio reseñasRepositorio ;
+    
+    public ReseñasControlador (ReseñasRepositorio reseñasRepertorio) {
+        this.reseñasRepositorio= reseñasRepertorio;
+    }
+    
+     public void listar(Context ctx) throws SQLException {
+        ctx.json(reseñasRepositorio.listar());
+    }
+       public void crear(Context ctx) throws SQLException {
+        // Usando un formulario
+        reseñasRepositorio.crear(ctx.formParam("descripcion", String.class).get() ); 
+        Reseña r  = ctx.bodyAsClass(Reseña.class);            
+        reseñasRepositorio.crear(r.getDescripcion());
+        
+        ctx.status(201);
+    }
+       
+       
+        public void borrar(Context ctx) throws SQLException, ReseñaNoEncontradoException {
+        reseñasRepositorio.borrar(reseñasRepositorio.obtener(ctx.pathParam("idReseña", Integer.class).get()));
+        ctx.status(204);
+    }
+        
+        
+        
+       public void modificar(Context ctx) throws SQLException,ReseñaNoEncontradoExcepcion, ReseñaNoEncontradoException {
+        Reseña reseña = reseñasRepositorio.obtener(ctx.pathParam("idUsuario", Integer.class).get());
+        // usando un formulario
+        reseña.setDescripcion(ctx.formParam("descripcion", String.class).get());
+        Reseña r  = ctx.bodyAsClass(Reseña.class);            
+        reseñasRepositorio.crear(r.getDescripcion());
+        reseñasRepositorio.modificar(reseña);
+        ctx.status(204);
+    }
+}
---
+++ PochocloCritics/src/main/java/org/pochocloapps/pochoclocritics/repositorios/ReseñasRepositorio.java
@@ -0,0 +1,116 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.pochocloapps.pochoclocritics.repositorios;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.pochocloapps.pochoclocritics.modelos.Reseña;
+   import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+;
+import org.pochocloapps.pochoclocritics.modelos.Reseña;
+import org.pochocloapps.pochoclocritics.modelos.Reseña;import org.pochocloapps.pochoclocritics.modelos.Reseña;
+/**
+ *
+ * @author Rosi-PC
+ */
+public class ReseñasRepositorio {
+     private final Connection conexion;
+  
+       public ReseñasRepositorio(Connection conn) throws SQLException{
+        
+        this.conexion= conn;
+        Statement consulta= conexion.createStatement();
+        consulta.execute("CREATE TABLE IF NOT EXISTS Reseña (idReseña SERIAL PRIMARY KEY,descripcion TEXT, idPelicula TEXT)");
+        consulta.close();
+    
+    }
+       
+        public void crear(String descripcion) throws SQLException {
+        PreparedStatement consulta = conexion.prepareStatement("INSERT INTO Reseña (descripcion) VALUES (?)");
+        consulta.setString(1, descripcion);
+        consulta.executeUpdate();  //Metodo de prepareStatement (a la vez de Statement "interface padre") que realiza una consulta. Utilizado para create, drop, insert, update, delete etc.
+        consulta.close();
+    }
+   
+    public void borrar(Reseña reseña )throws SQLException, ReseñaNoEncontradoException{
+        PreparedStatement consulta = conexion.prepareStatement("");
+        consulta.setInt(1, reseña.getIdReseña());
+        try{    
+            if (consulta.executeUpdate() == 0) throw new ReseñaNoEncontradoException() ;
+        }finally{
+                consulta.close();
+                
+                }
+    
+    }
+    public List<Reseña> listar()  throws SQLException{
+        List<Reseña> reseña = new ArrayList< >() ;
+          Statement consulta = conexion.createStatement(); //createStatement utilizado para crear declaraciones (Statement). Statement se utiliza para crear consultas en la BD.
+        ResultSet resultado = consulta.executeQuery("SELECT idReseña,descripcion, isPelicula FROM Reseña"); //executeQuery, método de Statement que es utilizado para crear consultas a la BD tipo SELECT. Retorna un Resulset que se puede utilizar para obtener todos los registros de una tabla.
+        while (resultado.next()) {
+            reseña.add(
+                new Reseña(
+                    resultado.getInt("idReseña"),
+                    resultado.getString("descripcion") 
+                   
+                    )
+            );
+        }
+               resultado.close(); //Método de Conection para cerrar la coneccion del objeto y del Resultset.
+        consulta.close();
+        return reseña;
+    }
+    public void crear(String descripcion, int idPelicula) throws SQLException {
+        PreparedStatement consulta = conexion.prepareStatement("INSERT INTO Reseña (descripcion , idPelicula) VALUES (?, ?, ?, ?, ?, ?)");
+        consulta.setString(1, descripcion);
+       // consulta.setInt(2, idPelicula);
+        consulta.executeUpdate();  //Metodo de prepareStatement (a la vez de Statement "interface padre") que realiza una consulta. Utilizado para create, drop, insert, update, delete etc.
+        consulta.close();
+    }
+   
+public void modificar (Reseña reseña ) throws SQLException,ReseñaNoEncontradoException  {
+    PreparedStatement consulta = conexion.prepareStatement("UPDATE Reseña SET descripcion = ?");
+        consulta.setString(1, reseña.getDescripcion());
+      //  consulta.setInt(2, reseña.getidpelicula);
+       
+        try {
+            if (consulta.executeUpdate() == 0) throw new ReseñaNoEncontradoException();
+        }
+        finally {
+            consulta.close();
+        }
+    }
+public Reseña  obtener(int idReseña) throws SQLException, ReseñaNoEncontradoException {
+        PreparedStatement consulta = conexion.prepareStatement("SELECT idReseña, descripcion FROM reseña WHERE idReseña = ?"); //PreparedStatement utilizado para realizar consultas parametrizadas. Subinterface de Statement.                                                                                                                                       //Se utiliza prepareStatement (Metodo de Conection) para obtener el objeto PreparedStatement.
+        consulta.setInt(1, idReseña);            //Ajusta el valor entero al parametro dado. Este caso el 1 (identificador).                                                                                                  
+        ResultSet resultado = consulta.executeQuery(); //Puntero que apunta al primer registro de una tabla. Inicialmente esta antes de la primer fila.
+        try {
+            if (resultado.next()) { //.next es un metodo de Resulset que permite avanzar el puntero a la siguiente posicion.
+                return new Reseña(
+                        resultado.getInt("idReseña"), //Metodos get de ResulSet para obtener el contenido de la fila donde esta parado.
+                        resultado.getString("descripcion")
+                        
+                );
+            } else {
+                throw new ReseñaNoEncontradoException();
+            }
+        }
+        finally {
+            consulta.close();
+            resultado.close();
+        }
+    }
+}  
+
